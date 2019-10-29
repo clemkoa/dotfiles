e1f205b078ebc6890f5b6729445cdc2c41b8402c
Clement Joudet <clement.joudet>
2019-10-29 16:22:07 +0100

    README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dotfiles
+> command line productivity tools
+
+Install > `bash <(curl -L https://raw.githubusercontent.com/clemkoa/dotfiles/master/dotfiles.sh)`
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,1 @@
 export PURE_CMD_MAX_EXEC_TIME=1
-export PROMPT="%(?.%F{green}.%F{red})${PURE_PROMPT_SYMBOL:-❯}%f "
-export PATH=/usr/local/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:$PATH
